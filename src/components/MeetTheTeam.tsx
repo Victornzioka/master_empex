3c198a8c36f4ec0fc73ca7a95d71125f10cbfde7
Victor <victormuia8@gmail.com>
2024-09-12 13:03:50 +0300

    add avatar for team members waiting for actual photos

--- src/components/MeetTheTeam.tsx
+++ src/components/MeetTheTeam.tsx
@@ -1,9 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
-import avatar6 from "@/assets/avatar-6.png";
-import avatar7 from "@/assets/avatar-7.png";
-import avatar1 from "@/assets/avatar-1.png";
+import avatar from "@/assets/avatar.jpg";
 
 const MeetTheTeam = () => {
   return (
@@ -16,28 +14,28 @@ const MeetTheTeam = () => {
       </div>
       <div className="flex flex-col md:flex-row px-12 pb-12 items-center justify-center gap-6 text-black/40">
         <div className="w-[200px] h-[200px] flex flex-col gap-4 justify-center items-center bg-white p-2 rounded-lg">
-          <Image src={avatar6} alt="avatar" />
+          <Image src={avatar} alt="avatar" />
           <div className="">
             <h2 className="font-bold">Mr. Micheal Mwale</h2>
             <p>Director of Operations</p>
           </div>
         </div>
         <div className="w-[200px] h-[200px] flex flex-col gap-4 justify-center items-center bg-white p-2 rounded-lg">
-          <Image src={avatar1} alt="avatar" />
+          <Image src={avatar} alt="avatar" />
           <div className="">
             <h2 className="font-bold">Lorna Mumaraki</h2>
             <p>Warehouse Manager</p>
           </div>
         </div>
         <div className="w-[200px] h-[200px] flex flex-col gap-4 justify-center items-center bg-white p-2 rounded-lg">
-          <Image src={avatar1} alt="avatar" />
+          <Image src={avatar} alt="avatar" />
           <div className="">
             <h2 className="font-bold">Sheila Korir</h2>
             <p>Warehouse Manager</p>
           </div>
         </div>
         <div className="w-[200px] h-[200px] flex flex-col gap-4 justify-center items-center bg-white p-2 rounded-lg">
-          <Image src={avatar7} alt="avatar" />
+          <Image src={avatar} alt="avatar" />
           <div className="">
             <h2 className="font-bold">Morrison Naphas</h2>
             <p>Technical Support</p>
